7aadcdce46090e087644fe31897a5814d71a3bff
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2018-04-01 00:01:26 +0300

    try canny + hough lines.

--- get_squares.py
+++ get_squares.py
@@ -3,28 +3,39 @@ import cv2
 import sys
 import numpy
 
+def convertRhoThetaToCoordinates(rho, theta):
+    a = numpy.cos(theta)
+    b = numpy.sin(theta)
+    x0 = a*rho
+    y0 = b*rho
+    x1 = int(x0 + 1000 * (-b))
+    y1 = int(y0 + 1000 * (a))
+    x2 = int(x0 - 1000 * (-b))
+    y2 = int(y0 - 1000 * (a))
+    return ((x1, y1), (x2, y2))
+
 if len(sys.argv) < 2:
     print("You should run `./get_squares.py <image>`")
     sys.exit(-1)
 
 image_file = sys.argv[1]
+frameRaw = cv2.imread(image_file)
+frame = cv2.resize(frameRaw, (1280, 720))
 
-def showCorners(patternSize, flag):
-    frame = cv2.imread(image_file)
+if frame is None:
+    print("Frame is not found in the given file.")
+    sys.exit(-1)
 
-    if frame is None:
-        print("Frame is not found in the given file.")
-        sys.exit(-1)
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray, 150, 200, 3)
+lines = cv2.HoughLines(edges, 1, numpy.pi / 180, 170)
 
-    found, corners = cv2.findChessboardCorners(frame, patternSize, flag)
-    if found is False:
-       return
-    cv2.drawChessboardCorners(frame, patternSize, corners, found)
+for lineArr in lines:
+    for line in lineArr:
+        rho, theta = line
+        (p1, p2) = convertRhoThetaToCoordinates(rho, theta)
+        cv2.line(frame, p1, p2, (0, 255, 0), 2)
 
-    cv2.imshow('Size: %dx%d' % patternSize, frame)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-for i in range(3, 9):
-    for j in range(3, 9):
-        showCorners((i, j), cv2.CALIB_CB_FAST_CHECK)
+cv2.imshow("Resulting frame", frame)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
