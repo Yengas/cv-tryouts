c72ba185a12d56ba989c3524515e643e8f7a6b96
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2018-04-12 02:12:47 +0300

    try feature tracking for the board. doesnt work.

--- track_chess_board.py
+++ track_chess_board.py
@@ -83,6 +83,52 @@ class VideoStab:
         self.last_T = T
         return cv2.warpAffine(self.prev, T, self.size)
 
+class FeatureTracking:
+    def __init__(self, prev, size, features_options = (200, 0.01, 30), prev_gray=None):
+        # set the first frame as the previous and convert the color to gray
+        self.prev = prev.copy()
+        self.prev_gray = prev_gray.copy() if prev_gray is not None else cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
+
+        # set the size for the input and output image as a width/height tuple
+        self.size = size
+
+        # setup sift finder and matcher
+        self.sift = cv2.xfeatures2d.SIFT_create()
+        # FLANN parameters
+        FLANN_INDEX_KDTREE = 0
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks=50)   # or pass empty dictionary
+        self.flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+        self.prev_kps, self.prev_descs = self.sift.detectAndCompute(self.prev_gray, None)
+
+
+    def process(self, cur, cur_gray=None):
+        # set the first frame as the previous and convert the color to gray
+        cur = cur.copy()
+        cur_gray = cur_gray.copy() if cur_gray is not None else cv2.cvtColor(cur, cv2.COLOR_BGR2GRAY)
+
+        cur_kps, cur_descs = self.sift.detectAndCompute(cur_gray, None)
+        # TODO: try changing between prev and cur
+        matches = self.flann.knnMatch(cur_descs, self.prev_descs, k=2)
+
+        # ratio test as per Lowe's paper
+        matches = [m for m, n in matches if m.distance < 0.7 * n.distance]
+
+        if len(matches) > 300:
+            src_pts = np.float32([ cur_kps[m.queryIdx].pt for m in matches ]).reshape(-1,1,2)
+            dst_pts = np.float32([ self.prev_kps[m.trainIdx].pt for m in matches ]).reshape(-1,1,2)
+
+            homography, _ = cv2.findHomography(src_pts, dst_pts)
+            #homography = np.ones((3, 3), dtype=np.double)
+        else:
+            return None
+
+        self.prev, self.prev_gray = cur, cur_gray
+        self.prev_kps, self.prev_descs = cur_kps, cur_descs
+        return homography
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Given a video where the camera is static as possible, removes shakes and finds the chessboard'
@@ -102,18 +148,24 @@ if __name__ == '__main__':
         print('Could not read the first frame.')
         sys.exit(-1)
 
-    videostab = VideoStab(prev, (width, height))
+    #videostab = VideoStab(prev, (width, height))
+    tracking = FeatureTracking(prev, (width, height))
     pattern_size, board_size = (7, 7), (640, 640)
     last_homography, last_corners = None, None
+    last_camera_homography = np.ones((3, 3), dtype=np.double)
     frame = 0
 
     while video.isOpened():
         ret, cur = video.read()
         if ret is False:
             break
-        result = videostab.process(cur)
+        #result = videostab.process(cur)
+        camera_homography = tracking.process(cur)
+        if camera_homography is not None:
+            last_camera_homography = camera_homography
+        result = cur
 
-        if frame % 20 == 0:
+        if last_homography is None and frame % 20 == 0:
             found, homography, corners = findChessboardPlacement(result, pattern_size, board_size, None, cv2.CALIB_CB_FAST_CHECK)
 
             if found:
@@ -121,7 +173,7 @@ if __name__ == '__main__':
                 last_corners = corners
 
         if last_homography is not None:
-            board = cv2.warpPerspective(result, last_homography, board_size)
+            board = cv2.warpPerspective(result, last_homography * last_camera_homography, board_size)
             cv2.imshow('board', board)
 
         cv2.imshow('camera', result)
